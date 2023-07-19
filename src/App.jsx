ca79d80e749f99c011d14136a00436406bb742f9
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-07-20 08:13:44 +1000

    Create pages structure in src folder.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomeLayout } from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+  },
+  {
+    path: "/about",
+    element: (
+      <div>
+        <h1>about page</h1>
+      </div>
+    ),
+  },
+]);
+
 const App = () => {
-  return <h1>Jobify App</h1>;
+  return <RouterProvider router={router} />;
 };
 export default App;
