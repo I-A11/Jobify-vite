d277a235ca1f2fbcb70879a024fddd841c218a2c
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-07-21 14:17:11 +1000

    Create error page and style it.

--- src/assets/wrappers/ErrorPage.js
+++ src/assets/wrappers/ErrorPage.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Wrapper = styled.main`
   min-height: 100vh;
@@ -9,15 +9,13 @@ const Wrapper = styled.main`
   img {
     width: 90vw;
     max-width: 600px;
-    display: block;
-    margin-bottom: 2rem;
-    margin-top: -3rem;
+    margin: -3rem 0 2rem;
   }
   h3 {
     margin-bottom: 0.5rem;
   }
   p {
-    line-height: 1.5;
+    line-height: 1.5rem;
     margin-top: 0.5rem;
     margin-bottom: 1rem;
     color: var(--text-secondary-color);
