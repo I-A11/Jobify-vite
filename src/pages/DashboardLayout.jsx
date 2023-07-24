c9ecf06720f4470f27df42dae45a7497a19fe4ab
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-07-24 10:13:33 +1000

    Create dashboard layout.

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,4 +1,21 @@
+import { Outlet } from "react-router-dom";
+import Wrapper from "../assets/wrappers/Dashboard";
+import { BigSidebar, Navbar, SmallSidebar } from "../component";
+
 const DashboardLayout = () => {
-  return <div>DashboardLayout</div>;
+  return (
+    <Wrapper>
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <Navbar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
+  );
 };
 export default DashboardLayout;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,7 +9,6 @@ const Login = () => {
       <form className="form">
         <Logo />
         <h4>Login</h4>
-
         <FormRow type="email" name="email" defaultValue="john@gmail.com" />
         <FormRow type="password" name="password" defaultValue="secret123" />
         <button type="submit" className="btn btn-block">
